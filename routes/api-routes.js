b2703ca8e089f4ff370b4f48f6c735a2ff9c83e8
Tony Tran <codemonkey713@gmail.com>
2021-02-20 18:20:02 -0600

    cleaned up server code and completed the delete note API call

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,4 @@
-// const tableNote = require('../db/table-note');
-
 const fs = require('fs');
-// const { finished } = require('stream');
 const data = fs.readFileSync('./db/db.json');
 let notesData = JSON.parse(data);
 
@@ -10,10 +7,10 @@ module.exports = (app) => {
     res.json(notesData);
   });
   
+  // function to write to JSON file anytime notes are updated, add, removed from list
   function writeToJSONfile(){
     const noteChanged = JSON.stringify(notesData, null, 2);
     fs.writeFile('./db/db.json', noteChanged, finished);
-    
     function finished(err){
       console.log('JSON file updated!');
     }
@@ -21,46 +18,25 @@ module.exports = (app) => {
   
   app.post('/api/notes', (req, res) => {
     const newNotes = req.body;
-
     notesData.push(req.body);
     writeToJSONfile();
-    // const jnotes = JSON.stringify(notesData, null, 2);
-    // fs.writeFile('./db/db.json', jnotes, finished);
-    
-    // function finished(err){
-    //   console.log('JSON file updated!');
-    // }
-
-    console.log(jnotes);
     res.json(newNotes);
   });
 
+  // API call for deleting a note from the list or array
   app.delete('/api/notes/:id', (req, res) => {
-    // this is the function for deleting a note from the 
     const deleteID = req.params.id;
-    console.log(deleteID);
-    
-    
     for (let i = 0; i < notesData.length; i++) {
       if (deleteID === notesData[i].id) {
-        // console.log(notesData[i]);
-        notesData.splice(i, i);
-        console.log(notesData);
-        // writeToJSONfile();
-        // const deletedNotes = JSON.stringify(notesData, null, 2);
-        // fs.writeFile('./db/db.json', deletedNotes, finished);
-        // function finished(err){
-        //   console.log('JSON file updated!');
-        // }
+        notesData.splice(i, 1);
+        writeToJSONfile();
       }
     }
-
     res.end();
   });
 
   app.post('/api/clear', (req, res) => {
     notesData.length = 0;
-
     res.json({ ok: true });
   });
 }
